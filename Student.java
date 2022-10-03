892177b78ab05ad48d0173091d070c7c2d64c007
J-Rolf <jacksynrolf@gmail.com>
2022-10-03 08:50:58 -0500

    course

--- Student.java
+++ Student.java
@@ -1,5 +1,41 @@
 import java.io.Serializable;
 
 public class Student implements Serializable {
+    //instance variables
+    private String name;
+    private int level;
+    private ArrayList <Course> courses;
+
+    //constructors
+    public Student (String name, int level){
+        this.name = name;
+        this.level = level;
+        courses = new ArrayList<>();
+    }
+
+    //getters
+    public String getName(){
+        return name;
+    }
+    public int getLevel(){
+        return level;
+    }
+    public ArrayList <Course>(){
+
+    }
+
+    //setters
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setLevel (int level){
+        this.level = level;
+    }
+
+    //addCourse
     
+
+    //removeCourse
+
+
 }
